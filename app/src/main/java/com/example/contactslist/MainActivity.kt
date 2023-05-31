165e1d48890c2a8678a055ceff7c7e07fab75e25
Chepkemboi <Chepkemboirabecca@gmail.com>
2023-05-31 19:53:17 +0300

    created a contact list

--- app/src/main/java/com/example/contactslist/MainActivity.kt
+++ app/src/main/java/com/example/contactslist/MainActivity.kt
@@ -10,13 +10,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val contacts = mutableListOf(
-            ContactData( "Lyne", "johndoe@example.com", "+254789453", imageResId = R.drawable.avatar),
-            ContactData("Irene",  "irenemusembi@gmail.com", "+2567894532", imageResId = R.drawable.avatar),
-            ContactData("Mercy","yvonne@gmail.com","0785645342", imageResId = R.drawable.avatar),
-            ContactData("Rose" ,"Ann", "annkelly@gmail.com", imageResId = R.drawable.avatar ),
-            ContactData( "Willy", "willysifuna@gmail.com",  "075649384", imageResId = R.drawable.avatar),
-            ContactData("Eunice", "eunicemusembi@gmai.com", "0798032356", imageResId = R.drawable.avatar),
-            ContactData( "Daniel", "musembidaniel@gmail.com",  "0789457458", imageResId = R.drawable.avatar)
+            ContactData( "Mercy", "mercy@example.com", "+254789453", imageResId = R.drawable.avatar),
+            ContactData("Clarah",  "clarah@gmail.com", "+2567894532", imageResId = R.drawable.avatar),
+            ContactData("Ann","ann@gmail.com","0785645342", imageResId = R.drawable.avatar),
+            ContactData("Issabellah" ,"issabellah@gmail.com", "072536378", imageResId = R.drawable.avatar ),
+            ContactData( "Vince", "vince@gmail.com",  "075649384", imageResId = R.drawable.avatar),
+            ContactData("Ambrose", "ambrose@gmai.com", "0798032356", imageResId = R.drawable.avatar),
+            ContactData( "Samwel", "samwel@gmail.com",  "0789457458", imageResId = R.drawable.avatar)
         )
         val recyclerView = findViewById<RecyclerView>(R.id.rvContact)
         recyclerView.layoutManager = LinearLayoutManager(this)
